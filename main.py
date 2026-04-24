457c21179cf36a9351eae0105820a6ce2cdf2037
Michael Rodriguez <paininquires@gmail.com>
2026-04-23 23:44:45 -0400

    texture id matching

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter
 from tkinter import filedialog
 from pathlib import Path
@@ -7,18 +8,27 @@ import json
 tkinter.Tk().withdraw()
 
 def init(data):
-    """initializes an empty texture pack"""
+    """initializes an empty texture pack folder"""
 
     data_path = Path(data)
 
-    pack_folder = Path("./packs/") / data_path.name
-    pack_folder.mkdir(exist_ok=True)
+    pack_directory = Path("./packs/") / data_path.name
+    pack_directory.mkdir(exist_ok=True)
 
-    pack_assets = pack_folder / "assets"
+    pack_assets = pack_directory / "assets"
     pack_assets.mkdir(exist_ok=True)
 
+    pack_minecraft = pack_assets / "minecraft"
+    pack_minecraft.mkdir(exist_ok=True)
+
+    packgen = pack_assets / "packgen"
+    packgen.mkdir(exist_ok=True)
+
+    pack_minecraft_items = pack_minecraft / "items"
+    pack_minecraft_items.mkdir(exist_ok=True)
+
     if (data_path / "pack.png").is_file():
-        shutil.copy(data_path / "pack.png", pack_folder)
+        shutil.copy(data_path / "pack.png", pack_directory)
 
     meta = {
         "pack": {
@@ -26,13 +36,28 @@ def init(data):
             "description": "1.21.11",
         }
     }
-    with open(pack_folder/"pack.mcmeta", "w", encoding='utf-8') as pack_meta:
+    with open(pack_directory/"pack.mcmeta", "w", encoding='utf-8') as pack_meta:
         json.dump(meta, pack_meta, indent = 4)
 
+    return data_path, pack_assets
 
-creation_data = filedialog.askdirectory()
+input_directory = filedialog.askdirectory()
 
-if creation_data:
-    init(creation_data)
-else:
-    print("fuck you too")
+if not input_directory:
+    sys.exit("fuck you too")
+
+input_path, assets = init(input_directory)
+
+def locate_texture(skyblock_id):
+    """locates the texture for the id"""
+    for texture_file in input_path.iterdir():
+        if texture_file.stem.upper() == skyblock_id:
+            print(texture_file.stem)
+
+
+with open("./id_models.json", "r", encoding='utf-8') as id_models_json:
+    id_models = json.load(id_models_json)
+
+for model_key, model_value in id_models.items():
+    for item_id in model_value["skyblock_ids"]:
+        locate_texture(item_id)
